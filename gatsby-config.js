bdca51f2756c1ec6389f8792ddbd90f14f31ab40
Tomasz Kaminski <tomasz.kaminski@euvic.pl>
2019-07-06 16:36:23 +0200

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -15,6 +16,20 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: "gatsby-source-strapi",
+      options: {
+        apiURL: "http://localhost:1337",
+        contentTypes: [
+          // List of the Content Types you want to be able to request from Gatsby.
+          "article",
+          "user",
+          "tag",
+          "category",
+        ],
+        queryLimit: 1000,
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
