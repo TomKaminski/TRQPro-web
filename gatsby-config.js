758e6bb63b9a58929e14b54be04abc3b574f42fe
Tomasz Kaminski <tomasz.kaminski@euvic.pl>
2019-07-26 20:16:30 +0200

    fixed last slash

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
       resolve: "gatsby-source-strapi",
       options: {
         apiURL: process.env.DEPLOY_URL
-          ? "https://trq-cms.herokuapp.com/"
+          ? "https://trq-cms.herokuapp.com"
           : "http://localhost:1337",
         contentTypes: [
           // List of the Content Types you want to be able to request from Gatsby.
