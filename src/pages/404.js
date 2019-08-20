6a66cac001b7855deb81d3008f00331c67c6c912
Tomasz Kaminski <tomasz.kaminski@euvic.pl>
2019-08-20 12:26:39 +0200

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,25 @@
 import React from "react"
 
-import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Link } from "gatsby"
+import { Container } from "react-bootstrap"
+import errorImg from "../images/error-404.svg"
+import logoImg from "../images/brand_logo.png"
+import "../styles/404.scss"
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  <Container>
+    <SEO title="404: Nie znaleziono" />
+    <img src={logoImg} alt="logo" className={"logo"} />
+    <div className={"container-inner"}>
+      <img src={errorImg} alt="404" className={"error"} />
+      <h4>What on TRQPro are you doing here?!</h4>
+      <p>
+        Dziwne.. Wygląda na to, ze strona którą próbujesz otworzyć nie istnieje.
+      </p>
+      <Link to="/">zabierz mnie stąd</Link>
+    </div>
+  </Container>
 )
 
 export default NotFoundPage
