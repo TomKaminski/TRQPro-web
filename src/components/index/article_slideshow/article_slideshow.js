d11f144f884f096e87b2df97fce85655130688fe
Tomasz Kaminski <>
2020-01-02 18:25:18 +0100

    initial carousel done

--- src/components/index/article_slideshow/article_slideshow.js
+++ src/components/index/article_slideshow/article_slideshow.js
@@ -3,6 +3,9 @@ import React from "react"
 import "../../../styles/index/articleSlideshow.scss"
 import ArticleSlideshowItem from "./article_slideshow_item"
 
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import { Carousel } from "react-responsive-carousel"
+
 export default class ArticleSlideshow extends React.Component {
   constructor(props) {
     super(props)
@@ -15,7 +18,24 @@ export default class ArticleSlideshow extends React.Component {
   render() {
     return (
       <div id="article-slideshow" className={"page-padding"}>
-        <ArticleSlideshowItem article={this.props.articles[1]} />
+        <Carousel
+          showThumbs={false}
+          infiniteLoop={true}
+          showArrows={false}
+          showStatus={false}
+          showIndicators={false}
+          transitionTime={1000}
+          interval={4000}
+          swipeable={true}
+          autoPlay={true}
+          emulateTouch
+        >
+          <ArticleSlideshowItem article={this.props.articles[0]} />
+          <ArticleSlideshowItem article={this.props.articles[1]} />
+          <ArticleSlideshowItem article={this.props.articles[2]} />
+          <ArticleSlideshowItem article={this.props.articles[3]} />
+          <ArticleSlideshowItem article={this.props.articles[4]} />
+        </Carousel>
         <div className={"bg"}></div>
       </div>
     )
