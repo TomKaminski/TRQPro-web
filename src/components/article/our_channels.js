76f02fd74ecb429ca88e60561256de93278bb6b7
Tomasz Kaminski <>
2020-03-02 13:27:29 +0100

    target blank for our channels

--- src/components/article/our_channels.js
+++ src/components/article/our_channels.js
@@ -9,56 +9,77 @@ const OurChannels = () => {
       <ul className="unstyled-list">
         <li>
           TRQPro Analizy BTC, Alty, Ico:{" "}
-          <a href="https://t.me/TRQProAnalizy">https://t.me/TRQProAnalizy</a>
+          <a href="https://t.me/TRQProAnalizy" target="_blank">
+            https://t.me/TRQProAnalizy
+          </a>
         </li>
         <li>
           TRQPro Dzwignia, BTC chat:{" "}
-          <a href="https://t.me/TRQProBTC">https://t.me/TRQProBTC</a>
+          <a href="https://t.me/TRQProBTC" target="_blank">
+            https://t.me/TRQProBTC
+          </a>
         </li>
         <li>
           TRQPro Mining:{" "}
-          <a href="https://t.me/TRQProMining">https://t.me/TRQProMining</a>
+          <a href="https://t.me/TRQProMining" target="_blank">
+            https://t.me/TRQProMining
+          </a>
         </li>
         <li>
           TRQPro ICO/IEO/Airdrop:{" "}
-          <a href="https://t.me/TRQProICO">https://t.me/TRQProICO</a>
+          <a href="https://t.me/TRQProICO" target="_blank">
+            https://t.me/TRQProICO
+          </a>
         </li>
         <li>
           TRQPro News:{" "}
-          <a href="https://t.me/TRQProNews">https://t.me/TRQProNews</a>
+          <a href="https://t.me/TRQProNews" target="_blank">
+            https://t.me/TRQProNews
+          </a>
         </li>
         <li>
           TRQPro Alty chat:{" "}
-          <a href="https://t.me/TRQProAlty">https://t.me/TRQProAlty</a>
+          <a href="https://t.me/TRQProAlty" target="_blank">
+            https://t.me/TRQProAlty
+          </a>
         </li>
         <li>
           TRQPro Forex:{" "}
-          <a href="https://t.me/TRQProForex">https://t.me/TRQProForex</a>
+          <a href="https://t.me/TRQProForex" target="_blank">
+            https://t.me/TRQProForex
+          </a>
         </li>
         <li>
           TRQPro Social:{" "}
-          <a href="https://t.me/TRQProSocial">https://t.me/TRQProSocial</a>
+          <a href="https://t.me/TRQProSocial" target="_blank">
+            https://t.me/TRQProSocial
+          </a>
         </li>
         <li>
-          TRQPro AT: <a href="https://t.me/TRQProAT">https://t.me/TRQProAT</a>
+          TRQPro AT:{" "}
+          <a href="https://t.me/TRQProAT" target="_blank">
+            https://t.me/TRQProAT
+          </a>
         </li>
       </ul>
       <ul className="unstyled-list">
         <li>
           Facebook:{" "}
-          <a href="https://www.facebook.com/TRQPro/?tn-str=k*F">
+          <a href="https://www.facebook.com/TRQPro/?tn-str=k*F" target="_blank">
             https://www.facebook.com/TRQPro/?tn-str=k*F
           </a>
         </li>
         <li>
           Grupa na Facebooku:{" "}
-          <a href="https://www.facebook.com/groups/TRQPro/">
+          <a href="https://www.facebook.com/groups/TRQPro/" target="_blank">
             https://www.facebook.com/groups/TRQPro/
           </a>
         </li>
         <li>
           Twitter:{" "}
-          <a href="https://twitter.com/TRQPro">https://twitter.com/TRQPro</a>
+          <a href="https://twitter.com/TRQPro" target="_blank">
+            https://twitter.com/TRQPro
+          </a>
         </li>
       </ul>
       <p className="call-to-join-channels">
