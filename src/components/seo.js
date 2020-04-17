0cf4114cea99850dd1e00061172974979ae3f50a
Tomasz Kaminski <>
2020-04-17 09:37:17 +0200

    seo improvements

--- src/components/seo.js
+++ src/components/seo.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-function SEO({ description, lang, meta, title, pathname, image: metaImage }) {
+function SEO({ description, lang, title, pathname, image: metaImage }) {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -67,12 +67,12 @@ function SEO({ description, lang, meta, title, pathname, image: metaImage }) {
           content: `website`,
         },
         {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
           property: `og:url`,
           content: canonical,
+        },
+        {
+          name: `twitter:card`,
+          content: `summary`,
         },
         {
           name: `twitter:creator`,
@@ -86,43 +86,27 @@ function SEO({ description, lang, meta, title, pathname, image: metaImage }) {
           name: `twitter:description`,
           content: metaDescription,
         },
-      ]
-        .concat(
-          metaImage
-            ? [
-                {
-                  property: "og:image",
-                  content: image,
-                },
-                {
-                  property: "og:image:width",
-                  content: metaImage.width,
-                },
-                {
-                  property: "og:image:height",
-                  content: metaImage.height,
-                },
-                {
-                  name: "twitter:card",
-                  content: "summary_large_image",
-                },
-              ]
-            : [
-                {
-                  name: "twitter:card",
-                  content: "summary",
-                },
-              ]
-        )
-        .concat(meta)}
+      ].concat(
+        metaImage
+          ? [
+              {
+                property: `og:image`,
+                content: image,
+              },
+              {
+                name: "twitter:card",
+                content: "summary_large_image",
+              },
+            ]
+          : [
+              {
+                name: "twitter:card",
+                content: "summary",
+              },
+            ]
+      )}
     />
   )
-}
-
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
 }
 
 SEO.propTypes = {
@@ -132,8 +116,6 @@ SEO.propTypes = {
   title: PropTypes.string.isRequired,
   image: PropTypes.shape({
     src: PropTypes.string.isRequired,
-    height: PropTypes.number.isRequired,
-    width: PropTypes.number.isRequired,
   }),
   pathname: PropTypes.string,
 }
